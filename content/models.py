89802fca1277483f6d0a718a7f054f93511234de
= <hasanqqsp@gmail.com>
2021-05-17 22:20:56 +0700

    updating models

--- content/models.py
+++ content/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils.text import slugify
+from content.utils import generate_id
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+class Article(models.Model):
+    article_id = models.CharField(unique=True, primary_key=True,editable=True,max_length=16)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    approved_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name='approved_by')
+    moderator_note = models.TextField(blank=True,null=True)
+    status_changed_by = models.ForeignKey(User,on_delete=models.CASCADE, null=True,blank=True,related_name='status_changed_by')
+    content = models.TextField()
+    category = models.ForeignKey(Category,null=True,blank=True,on_delete=models.CASCADE)
+    
--- user/models.py
+++ user/models.py
@@ -7,11 +7,16 @@ from django.db.models.signals import post_save #add this
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, null=True,on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='user_uploads/')
-    role = models.JSONField(default=list('moderator'))
+    avatar = models.ImageField(upload_to='user_uploads/',null=True,blank=True)
+    is_writer = models.BooleanField(default = True)
+    is_trusted_writer = models.BooleanField(default = False)
+    is_moderator = models.BooleanField(default = False)
+    is_admin = models.BooleanField(default = False)
+    is_disabled = models.BooleanField(default = False)
+    is_muted = models.BooleanField(default = False)
     bio = models.TextField(blank=True, null=True)
-    last_update = models.DateTimeField(auto_now_add=True)
-    
+    short_bio = models.CharField(blank=True, null=True, max_length=256)
+    registered = models.DateTimeField(auto_now_add=True)
     
     
     @receiver(post_save, sender=User)
